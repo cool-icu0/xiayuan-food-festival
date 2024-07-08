bdc2b2db1e30cbc80114ec3e3afa51c30cc413d0
Cool <990299103@qq.com>
2024-07-08 15:00:11 +0800

    feat："使用微服务nacos改造file和gateway"

---
+++ take-out/take_out_gateway/src/main/java/com/example/take_out_gateway/TakeOutGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.take_out_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeOutGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeOutGatewayApplication.class, args);
+    }
+
+}
